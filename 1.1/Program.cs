835945ccaccfd3522737f175acf98768c4ef8ecc
Elfern123 <45273768+Elfern123@users.noreply.github.com>
2025-10-05 19:23:57 +0300

    Add files via upload
    initial commit

---
+++ 1.1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+    var random = new Random();
+    const string characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    string randomString = string.Empty;
+
+    for (int i = 0; i < 5; i++) 
+    {
+        char charToAdd = characters[random.Next(characters.Length)];
+        randomString += charToAdd.ToString();
+    }
+while (true)
+{
+    Console.WriteLine(randomString);
+    Console.WriteLine(DateTime.Now);
+    System.Threading.Thread.Sleep(5000);
+}
+
